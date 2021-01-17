943720b3827165a2c36fe5a3740ecaedff473a91
Sean Chen <sean.chen@leocorn.com>
2021-01-17 22:40:32 +0000

    add more message, failed at TypeError!

--- main.py
+++ main.py
@@ -12,4 +12,14 @@ print( greeting )
 
 # collection user's name:
 name = input( 'What is your name: ' )
-print( 'Nice to meet you, ', name)
+print( 'Nice to meet you, ', name )
+
+# collect user's age
+age = input( "How old are you: " )
+
+# branch out using if statement
+if age <= 10:
+    print( 'Sorry ', name, '! We are not allowed to provide game for kids less then 11 years old!' )
+    exit()
+
+print( 'Hi ', name, ', Welcome to the game!' )
