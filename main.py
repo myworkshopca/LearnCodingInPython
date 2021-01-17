ec0999ad70366d894256023d6e602485003a0e56
Sean Chen <sean.chen@leocorn.com>
2021-01-17 22:06:39 +0000

    add fancy greeting print out

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+# here is the comment
 # my first ever code in Python!
 print('Hello World!')
+
+# ==============================
+# how to use variable?
+
+# define a variable is very simple:
+greeting = 'The fancy greeting!'
+
+print( greeting )
