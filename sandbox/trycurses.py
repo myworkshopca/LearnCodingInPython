0c6ca2c4f8d74f15c93965c43d6aa60477abca1b
Sean Chen <sean.chen@leocorn.com>
2021-01-20 23:02:32 +0000

    add more comments

--- sandbox/trycurses.py
+++ sandbox/trycurses.py
@@ -2,12 +2,22 @@ from curses import textpad
 import curses
 
 def main(stdscr):
-    curses.curs_set(0)
-    sh, sw = stdscr.getmaxyx()
-    box = [[3,3], [sh - 3, sw - 3]]
 
+    # set 0 to disable the flash.
+    curses.curs_set(0)
+
+    # get the size of the windown.
+    sh, sw = stdscr.getmaxyx()
+
+    # set up the game area.
+    # top left and bottom right
+    box = [[3,3], [sh - 3, sw - 3]]
+    # draw the rectangle as the game area.
     textpad.rectangle( stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
+
     stdscr.refresh()
+
+    # waiting for user input
     stdscr.getch()
 
 curses.wrapper(main)
