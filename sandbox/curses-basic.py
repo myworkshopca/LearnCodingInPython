f9851534b84b02ebec2391e967549db6c8f10d74
Sean Chen <sean.chen@leocorn.com>
2021-01-21 10:35:29 -0500

    show the process for timeout and game over message.

--- sandbox/curses-basic.py
+++ sandbox/curses-basic.py
@@ -10,11 +10,25 @@ def main(stdscr):
 
     # the y axis, starts from 1
     y = 1
+    # print the prompt message:
+    stdscr.addstr(0, 0, 'Process Q to exit!')
     while 1:
 
         userType = stdscr.getch()
         # getch will return a ascii code for each key,
-        stdscr.addstr(y, 1, chr(userType));
+        # the function chr to conver a ascii code to a character
+        msg = "{0} - {1}".format(userType, chr(userType))
+        stdscr.addstr(y, 1, msg);
         y += 1
+        if chr(userType) == 'Q':
+            # exit the while loop
+            stdscr.addstr(y, 1, 'Closing game ...')
+
+            # turn on the nodelay mode to use timeout
+            stdscr.nodelay(1)
+            stdscr.timeout(800)
+            # call getch in nodelay mode to sho the message.
+            stdscr.getch()
+            break
 
 curses.wrapper(main)
