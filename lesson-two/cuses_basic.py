0003c1ad9f8fe72e42fc2d871d451dea0fcf65f1
Sean Chen <sean.chen@leocorn.com>
2021-01-25 22:30:25 +0000

    some minor change

--- lesson-two/cuses_basic.py
+++ lesson-two/cuses_basic.py
@@ -3,8 +3,9 @@ import curses
 def main(stdscr):
 
     sh, sw = stdscr.getmaxyx()
+    msg = "Welcome to snake game" * 2
     # y-axis and x-axis
-    stdscr.addstr(sh // 2, sw // 2, 'Welcome to snake game!')
+    stdscr.addstr(sh // 2, sw // 2 - len(msg) // 2, msg)
 
     stdscr.getch()
 
