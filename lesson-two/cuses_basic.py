b0f986c986ece0e22b66d5663954569326038335
Sean Chen <sean.chen@leocorn.com>
2021-01-31 09:47:01 -0500

    start to paint the border

--- lesson-two/cuses_basic.py
+++ lesson-two/cuses_basic.py
@@ -3,9 +3,19 @@ import curses
 def main(stdscr):
 
     sh, sw = stdscr.getmaxyx()
-    msg = "Welcome to snake game" * 2
-    # y-axis and x-axis
-    stdscr.addstr(sh // 2, sw // 2 - len(msg) // 2, msg)
+    msg = "Welcome to keyboard encoding"
+    # print the welcome message y-axis and x-axis
+    stdscr.addstr(0, sw // 2 - len(msg) // 2, msg)
+
+    # paint the border.
+    # the top and bottom border.
+    for x in range(0, sw - 1):
+        stdscr.addstr(2, x, '+')
+        stdscr.addstr(sh - 3, x, '+')
+
+    for y in range(0, sh - 1):
+        stdscr.addstr(y, 1, "|")
+        stdscr.addstr(y, sw - 1, "|")
 
     stdscr.getch()
 
