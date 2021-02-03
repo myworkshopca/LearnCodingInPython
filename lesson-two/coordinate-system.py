cd0291cd97a0fa14eb5ca42c05c3d6d3454923b1
Sean Chen <sean.chen@leocorn.com>
2021-02-03 14:58:35 -0500

    try different chrs

--- lesson-two/coordinate-system.py
+++ lesson-two/coordinate-system.py
@@ -7,15 +7,20 @@ def window(stdscr):
     # get size of the screen.
     sh, sw = stdscr.getmaxyx()
 
+    # ┌ 9484
+    stdscr.addstr(0, 0, chr(9484))
+
     # paint the x axis.
-    for x in range(0, sw):
-        stdscr.addstr(0, x, "-")
+    for x in range(1, sw):
+        # ─ 9472
+        stdscr.addstr(0, x, chr(9472))
     # paint the ending 9658 - ►
     stdscr.addstr(0, sw - 1, chr(9658))
 
-    for y in range(0, sh):
-        stdscr.addstr(y, 0, "|")
-    # paint the ending "▼"
+    for y in range(1, sh):
+        # │ 9474
+        stdscr.addstr(y, 0, chr(9474))
+    # paint the ending ▼ 9660
     stdscr.addstr(sh - 1, 0, chr(9660))
 
     stdscr.getch()
