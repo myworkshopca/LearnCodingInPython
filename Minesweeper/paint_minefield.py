d2407b022d7f5ff9acc3c2bef536670845ef4b21
Sean Chen <sean.chen@leocorn.com>
2021-02-17 17:52:37 -0500

    backup for painting the minefield

--- Minesweeper/paint_minefield.py
+++ Minesweeper/paint_minefield.py
@@ -35,46 +35,14 @@ def sweeper(stdscr):
         # reset the column index.
         c = 0
 
-    # set current row and column.
-    r, c = 0, 0
     # paint the reverse cell for the first cell.
-    stdscr.addstr(field[r][c][0], field[r][c][1], cell_ch, curses.A_REVERSE)
-    nr, nc = 0, 0
+    stdscr.addstr(field[0][0][0], field[0][0][1], cell_ch, curses.A_REVERSE)
 
     # try move the cursors
     while True:
-        # collect user's input.
+
         user_key = stdscr.getch()
 
-        # exit when user press ESC
-        if user_key == 27:
-            break;
-
-        # decide the new head based on the direction
-        if user_key == curses.KEY_UP:
-            if r > 0:
-                nr = r - 1
-        elif user_key == curses.KEY_DOWN:
-            if r < field_size * 2 - 1:
-                nr = r + 1
-        elif user_key == curses.KEY_RIGHT:
-            if c < field_size * 2 - 1:
-                nc = c + 1
-        elif user_key == curses.KEY_LEFT:
-            if c > 0:
-                nc = c - 1
-
-        if nr == r and nc == c:
-            # nothing change,
-            continue
-        else:
-            # paint current spot normally
-            stdscr.addstr(field[r][c][0], field[r][c][1], cell_ch)
-            # paint the new spot reverse.
-            stdscr.addstr(field[nr][nc][0], field[nr][nc][1], cell_ch, curses.A_REVERSE)
-            # set the current spot to new spot.
-            r, c = nr, nc
-
-    #stdscr.getch()
+    stdscr.getch()
 
 curses.wrapper(sweeper)
