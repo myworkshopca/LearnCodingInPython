f4a7afcd08fe825807bf2b03d832d24547a28efe
Sean Chen <sean.chen@leocorn.com>
2021-02-17 15:26:32 -0500

    introduce the field size

--- Minesweeper/game.py
+++ Minesweeper/game.py
@@ -12,9 +12,10 @@ def sweeper(stdscr):
     cell_ch = chr(9786)
 
     # define the minefield.
+    field_size = 10
     #field = 
-    for y in range(center[0] - 5, center[0] + 5):
-        for x in range(center[1] - 5, center[1] + 5):
+    for y in range(center[0] - field_size, center[0] + field_size):
+        for x in range(center[1] - field_size * 2, center[1] + field_size * 2, 2):
             stdscr.addstr(y, x, cell_ch)
 
     stdscr.getch()
