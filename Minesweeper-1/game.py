d1be47fe917e43153f4f179076d5c6d26dbe2b5e
Sean Chen <sean.chen@leocorn.com>
2021-02-19 16:36:48 -0500

    first version of initfield function.

--- Minesweeper-1/game.py
+++ Minesweeper-1/game.py
@@ -1,5 +1,11 @@
 import curses
 
+def initfield(stdscr, center, size):
+
+  for y in range(center[0] - size[0] // 2, center[0] + size[0] // 2):
+    for x in range(center[1] - size[1], center[1] + size[1], 2):
+      stdscr.addstr(y, x, chr(9608))
+
 def square(stdscr):
 
   curses.curs_set(0)
@@ -10,9 +16,7 @@ def square(stdscr):
   # row, column pair
   size = [16, 50]
 
-  for y in range(center[0] - size[0] // 2, center[0] + size[0] // 2):
-    for x in range(center[1] - size[1], center[1] + size[1], 2):
-      stdscr.addstr(y, x, chr(9608))
+  initfield(stdscr, center, size)
 
   stdscr.getch()
 
