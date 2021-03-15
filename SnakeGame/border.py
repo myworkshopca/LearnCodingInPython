b5444091c53ef64e08294ea1ce21b34b3f1f14c6
Sean Chen <sean.chen@leocorn.com>
2021-03-14 23:51:02 -0400

    create separate function to paint border.

--- SnakeGame/border.py
+++ SnakeGame/border.py
@@ -1,6 +1,25 @@
 import curses
 
+def paint_border(stdscr, uly, ulx, lry, lrx, border_ch):
+
+    # paint the top and bottom border, loop x-axis.
+    # range includes the start number, but NOT include end number.
+    for x in range(ulx, lrx + 1):
+        # the top border.
+        stdscr.addstr(uly, x, border_ch)
+        # the bottom border.
+        stdscr.addstr(lry, x, border_ch)
+
+    # loop through y-axis, paint the left and right border.
+    for y in range(uly, lry + 1):
+        # paint the left border.
+        stdscr.addstr(y, ulx, border_ch)
+        # paint the right border.
+        stdscr.addstr(y, lrx, border_ch)
+
 def border(stdscr):
+
+    curses.curs_set(0)
 
     sh, sw = stdscr.getmaxyx()
 
@@ -11,21 +30,9 @@ def border(stdscr):
     # █ 9608 ◼ 9724
     border_ch = chr(9608)
 
-    # paint the top and bottom border, loop x-axis.
-    # range includes the start number, but NOT include end number.
-    for x in range(margin_x, sw - margin_x):
-        # the top border.
-        stdscr.addstr(margin_y, x, border_ch)
-        # the bottom border.
-        stdscr.addstr(sh - margin_y, x, border_ch)
-
-    # loop through y-axis, paint the left and right border.
-    for y in range(margin_y, sh - margin_y):
-        # paint the left border.
-        stdscr.addstr(y, margin_x, border_ch)
-        # paint the right border.
-        stdscr.addstr(y, sw - margin_x - 1, border_ch)
-
+    paint_border(stdscr, margin_y, margin_x, sh - margin_y, sw - margin_x, border_ch)
+    stdscr.getch()
+    paint_border(stdscr, margin_y, margin_x, sh - margin_y, sw - margin_x, " ")
     stdscr.getch()
 
 curses.wrapper(border)
